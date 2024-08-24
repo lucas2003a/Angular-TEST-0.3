b9b27d0c51f010ac97ecf2b0c02f02d9a7b87026
lucas2003a <lucasatuncar1@gmail.com>
2024-08-23 23:19:20 -0500

    routes·

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ShopComponent } from './components/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:AppComponent},
+  {path:"shop", component:ShopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
